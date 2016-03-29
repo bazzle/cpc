78592aa704b6fc2180de234958a5a48d1d782e1f
Barry Richards <barry@br-webdesigner.com>
2016-03-29 02:15:48 +0100

    menu and styling update

--- js/main.js
+++ js/main.js
@@ -4,14 +4,44 @@ $(document).ready(function(){
 	
 	// menu
   
+  var themenu = $('.nav').html();
+  $('.menu__inner').html(themenu);
   
-  $('.header__button').on('click',function(){
-    $('.menu').animate({
-      width:'30%'
-    });
+  
+  var menuopen = false;
+  
+  $('.nav__button').on('click',function(){
+    
+    if ( !menuopen ){
+    
+        $('.menu').animate({
+          width:'30%'
+        },600,function(){
+          $('header .nav__button').fadeOut(600);
+          $('.menu__inner').fadeIn(600).find('.nav__button').html(' <i class="fa fa-close"></i> Close menu ');
+        });
+        menuopen = true;
+    
+    } else {
+        
+        $('.menu__inner').fadeOut(600,function(){
+          $('.menu').animate({
+            width:'0'
+          },600,function(){
+            $('.menu__inner').find('.nav__button').html(' <i class="fa fa-bars"></i> Menu ');
+            $('header .nav__button').fadeIn(600);
+          });
+        });
+      
+      menuopen = false;
+        
+    }
+    
   });
   
+  
 	
+  
 	
 	// convert all SVG's into png's for browsers that don't support svg's
 	
