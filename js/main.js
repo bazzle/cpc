60629e83bf41914260a83b609385513a88aa4ed0
Barry Richards <barry@br-webdesigner.com>
2016-04-20 21:06:02 +0100

    redone nav

--- js/main.js
+++ js/main.js
@@ -2,43 +2,63 @@
 $(document).ready(function(){
 	
 	
-	// menu
+	// menu  
   
-  var themenu = $('.nav').html();
-  $('.menu__inner').html(themenu);
-  
-  
-  var menuopen = false;
-  
-  $('.nav__button').on('click',function(){
+  function burgermenu(){
     
-    if ( !menuopen ){
+    
+    var themenu = $('.header__nav').html();
+    $('.menu__inner').html(themenu);
+  
+  
+    var menuopen = false;
+  
+    $('.nav__button').on('click',function(){
+    
+      if ( !menuopen ){
 
-        $('header .nav__button').fadeOut(200,function(){
-          $('.menu').animate({
-            width:'300px',
-          },600,'easeInOutExpo',function(){
-            $('.menu__inner').fadeIn(200).find('.nav__button').html(' <i class="fa fa-close"></i> Close menu ');
+          $('header .nav__button').fadeOut(200,function(){
+            $('.menu').animate({
+              width:'300px',
+            },600,'easeInOutExpo',function(){
+              $('.menu__inner').fadeIn(200).find('.nav__button').html(' <i class="fa fa-close"></i> Close menu ');
+            });
           });
-        });
-        menuopen = true;
+          menuopen = true;
     
-    } else {
+      } else {
         
-        $('.menu__inner').fadeOut(200,function(){
-          $('.menu').animate({
-            width:'0'
-          },600,'easeInOutExpo',function(){
-            $('.menu__inner').find('.nav__button').html(' <i class="fa fa-bars"></i> Menu ');
-            $('header .nav__button').fadeIn(200);
+          $('.menu__inner').fadeOut(200,function(){
+            $('.menu').animate({
+              width:'0'
+            },600,'easeInOutExpo',function(){
+              $('.menu__inner').find('.nav__button').html(' <i class="fa fa-bars"></i> Menu ');
+              $('header .nav__button').fadeIn(200);
+            });
           });
-        });
       
-      menuopen = false;
+        menuopen = false;
         
+      }
+    
+    });
+    
+    
+  }
+  
+  
+  
+  
+  enquire.register("screen and (max-width:700px)", {
+    
+    match : function() {
+    
+      burgermenu();
+      
     }
     
   });
+  
   
   
   
